cda5dbe3ba59860a4f3f8b315c20e7afd9f97e9a
Neha Verma <nehaverma@NehAshuMac.home>
2015-04-06 00:31:02 +0100

    base mvc config

---
+++ src/main/java/com/ib/nex/Application.java
@@ -0,0 +1,30 @@
+package com.ib.nex;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Application extends AbstractHandler {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8100);
+        server.setHandler(new Application());
+        server.start();
+        server.join();
+    }
+
+    @Override
+    public void handle(final String target, final Request baseRequest,
+                       final HttpServletRequest request,
+                       final HttpServletResponse response) throws IOException, ServletException {
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+}
